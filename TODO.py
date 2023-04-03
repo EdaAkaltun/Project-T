407f94ffc71d1aa37be0ae1375c539e964290557
Edster <74716069+EdaAkaltun@users.noreply.github.com>
2023-04-04 01:01:36 +0200

    Frontend

---
+++ TODO.py
@@ -0,0 +1,8 @@
+# https://wow.knmi.nl/ for temperature
+# IFC Load in issue
+# Karamba issue
+# Climada setup/download
+# rewrite script to do all ground floor columns
+# rewrite script to do for 4 risk amounts
+# front end tkinter
+# 

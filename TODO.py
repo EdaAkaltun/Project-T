3a6d543bea9395a5f4fff2af1f5d0b06c460b269
Edster <74716069+EdaAkaltun@users.noreply.github.com>
2023-04-06 16:56:15 +0200

    Frontendoverhaul

--- TODO.py
+++ TODO.py
@@ -2,7 +2,6 @@
 # IFC Load in for all columns
 # Karamba vs calc issue
 # rewrite script to do all ground floor columns
-# rewrite script to do for 4 risk amounts
 # do the diff load cases for diff columns
 # rewrite script to work in frontend
 # front end tkinter

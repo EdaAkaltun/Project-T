a6ebd537633426639680f87ae7b1511877f5b52a
Edster <74716069+EdaAkaltun@users.noreply.github.com>
2023-04-06 13:02:13 +0200

    Different connections implementation & loop for different flood cases

--- TODO.py
+++ TODO.py
@@ -1,8 +1,10 @@
 # https://wow.knmi.nl/ for temperature
-# IFC Load in issue
-# Karamba issue
-# Climada setup/download
+# IFC Load in for all columns
+# Karamba vs calc issue
 # rewrite script to do all ground floor columns
 # rewrite script to do for 4 risk amounts
+# do the diff load cases for diff columns
+# rewrite script to work in frontend
 # front end tkinter
-# 
+# thesis report
+# find out how temperature can be assessed

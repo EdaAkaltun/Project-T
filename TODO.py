87697478cfd657a8223ac03fe6dfc5a6a5c5006f
Edster <74716069+EdaAkaltun@users.noreply.github.com>
2023-06-18 10:55:59 +0200

    Cleanups

--- TODO.py
+++
@@ -1,9 +0,0 @@
-# https://wow.knmi.nl/ for temperature
-# IFC Load in for all columns
-# Karamba vs calc issue
-# rewrite script to do all ground floor columns
-# do the diff load cases for diff columns
-# rewrite script to work in frontend
-# front end tkinter
-# thesis report
-# find out how temperature can be assessed
